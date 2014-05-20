cf34b6139064bde126994295c8e342318f089797
FreeToGo@gmail.com <FreeToGo@gmail.com@08f0998b-4142-7e9d-4870-94cb81243daf>
2014-05-20 17:35:03 +0000

    ...

--- trunk/src/setenvWin32.bat
+++ trunk/src/setenvWin32.bat
@@ -1,4 +1,3 @@
-SET SVNEDITOR=/usr/bin/nano
 SET PYTHONPATH=C:\Python27_32
 SET OLDPATH=%PATH%
 SET PATH="%PYTHONPATH%;C:\Program Files (x86)\mingw-w64\i686-4.9.0-posix-dwarf-rt_v3-rev1\mingw32\bin;c:\swig;c:\cygwin64\bin"
