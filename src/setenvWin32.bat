40c9a58776d58a6829871fa9f792e45f023a31be
freetogo@gmail.com <freetogo@gmail.com@08f0998b-4142-7e9d-4870-94cb81243daf>
2014-06-05 11:47:21 +0000

    ...

--- src/setenvWin32.bat
+++ src/setenvWin32.bat
@@ -1,6 +1,7 @@
 SET PYTHONPATH=C:\Python27_32
 SET OLDPATH=%PATH%
 SET PATH="%PYTHONPATH%;C:\Program Files (x86)\mingw-w64\i686-4.9.0-posix-dwarf-rt_v3-rev1\mingw32\bin;c:\swig;c:\cygwin64\bin"
+gcc --version
 %PYTHONPATH%\python --version
 %PYTHONPATH%\python setup.py clean
 %PYTHONPATH%\python setup.py build
